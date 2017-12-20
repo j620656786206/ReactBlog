53487e820a333284ed62bf62afcacf35a795ed85
j620656786206 <j620656786206@gmail.com>
2017-12-20 17:40:57 -0600

    128. Rendering a List of Posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'https://blog-cwyualex.herokuapp.com/api';
-const API_KEY = '?mochaisbest2017';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=mochaisbest2017';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
